fb2853d78b85ccc5f2f95c51f3d1c681598c859d
Christoph Renzing <64863686+ChRen95@users.noreply.github.com>
2022-10-14 14:05:28 +0200

    Fix bug in profile cutting (#16)
    * Fixed bug for profile seperation
    
    * Uped version number
    
    * massage your code ;)
    
    * added distance for final check as fraction
    
    Co-authored-by: Max Weiner <max.weiner@imf.tu-freiberg.de>

--- pyroll/core/shapes.py
+++ pyroll/core/shapes.py
@@ -1,8 +1,9 @@
 from typing import Iterable
 
 import numpy as np
-from shapely.geometry import Polygon, LineString, MultiLineString
+from shapely.geometry import Polygon, LineString, MultiLineString, Point
 from shapely.ops import linemerge
+import logging
 
 _RECTANGLE_CORNERS = np.asarray([
     (-0.5, -0.5),
@@ -104,5 +105,35 @@ LineString._repr_pretty_ = polygon_repr_pretty
 
 def linemerge_if_multi(lines):
     if isinstance(lines, MultiLineString) or isinstance(lines, Iterable):
-        return linemerge(lines)
+        merged_line = linemerge(lines)
+
+        if isinstance(merged_line, MultiLineString):
+
+            logging.getLogger(__name__).warning(
+                "Discontinuous LineStrings for profile separation. Doing point wise comparison to resolve."
+            )
+
+            line_lengths = [line.length for line in merged_line.geoms]
+            index_of_continuous_line = np.argmax(line_lengths)
+            continuous_linestring = merged_line[index_of_continuous_line]
+
+            discontinuous_lines = list(lines)
+            discontinuous_lines.pop(index_of_continuous_line)
+
+            points_representation_of_discontinuous_lines = [Point(p) for l in discontinuous_lines for p in l]
+
+            distances_of_points_to_continuous_line = [continuous_linestring.distance(p) for p in
+                                                      points_representation_of_discontinuous_lines]
+
+            if max(distances_of_points_to_continuous_line) <= line_lengths[index_of_continuous_line] / 1000:
+                return continuous_linestring
+
+            else:
+                logging.getLogger(__name__).error(
+                    f"Could not resolve discontinuous LineStrings for profile separation."
+                )
+                raise RuntimeError("Could not resolve discontinuous LineStrings for profile separation.")
+
+        return merged_line
+
     return lines
