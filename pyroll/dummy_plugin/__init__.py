6ecff1c6c8b43e7f46d44077eb26a62ae73a185b
Max Weiner <max.weiner@imf.tu-freiberg.de>
2022-11-10 10:21:27 +0100

    remove dummy_plugin

--- pyroll/dummy_plugin/__init__.py
+++
@@ -1,7 +0,0 @@
-# this is a dummy plugin for testing purposes that is not included in the dist package
-from pyroll.core import RollPass
-
-
-@RollPass.roll_force
-def roll_force(roll_pass: RollPass):
-    return 42
