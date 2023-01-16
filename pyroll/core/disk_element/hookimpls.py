34a43376d35b5d7d9ddd664bd357363f66605042
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-01-16 09:16:30 +0100

    move disk element hooks from pyroll-thermal-2d

--- pyroll/core/disk_element/hookimpls.py
+++ pyroll/core/disk_element/hookimpls.py
@@ -15,3 +15,13 @@ def out_t(self: DiskElement.OutProfile):
 @DiskedUnit.disk_element_count
 def no_disks(self: DiskedUnit):
     return 0
+
+
+@DiskElement.duration
+def disk_duration(self: DiskElement):
+    return self.parent().duration / self.parent().disk_element_count
+
+
+@DiskElement.length
+def disk_length(self: DiskElement):
+    return self.parent().length / self.parent().disk_element_count
