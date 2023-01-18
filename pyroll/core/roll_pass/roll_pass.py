41d7dc636c146daa81457f729d67c97561da57f2
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-01-18 16:11:57 +0100

    move deformation unit

--- pyroll/core/roll_pass/roll_pass.py
+++ pyroll/core/roll_pass/roll_pass.py
@@ -6,62 +6,12 @@ import numpy as np
 from shapely.affinity import translate, rotate
 from shapely.geometry import LineString, Polygon
 
-from ..unit import Unit
 from ..disk_element import DiskedUnit
 from ..hooks import Hook
 from ..profile import Profile as BaseProfile
 from ..roll import Roll as BaseRoll
 from ..rotator import Rotator
-
-
-class DeformationUnit(Unit):
-    strain = Hook[float]()
-    """Mean equivalent strain applied to the workpiece."""
-
-    strain_rate = Hook[float]()
-    """Mean equivalent strain rate."""
-
-    spread = Hook[float]()
-    """Coefficient of spread (change in width)."""
-
-    elongation = Hook[float]()
-    """Coefficient of elongation (change in length)."""
-
-    draught = Hook[float]()
-    """Coefficient of draught (change in height)."""
-
-    log_spread = Hook[float]()
-    """Log. coefficient of spread (change in width)."""
-
-    log_elongation = Hook[float]()
-    """Log. coefficient of elongation (change in length)."""
-
-    log_draught = Hook[float]()
-    """Log. coefficient of draught (change in height)."""
-
-    abs_spread = Hook[float]()
-    """Absolute spread (change in width)."""
-
-    abs_elongation = Hook[float]()
-    """Absolute elongation (change in length)."""
-
-    abs_draught = Hook[float]()
-    """Absolute draught (change in height)."""
-
-    rel_spread = Hook[float]()
-    """Relative spread (change in width)."""
-
-    rel_elongation = Hook[float]()
-    """Relative elongation (change in length)."""
-
-    rel_draught = Hook[float]()
-    """Relative draught (change in height)."""
-
-    contact_area = Hook[float]()
-    """Area of contact of the workpiece to the rolls."""
-
-    free_surface_area = Hook[float]()
-    """Area of free surface."""
+from .deformation_unit import DeformationUnit
 
 
 class RollPass(DiskedUnit, DeformationUnit):
