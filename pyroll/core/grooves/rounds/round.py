376d2538d2e703dd4c499b014873c465899a9d41
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-02-10 09:44:02 +0100

    add more def options to round

--- pyroll/core/grooves/rounds/round.py
+++ pyroll/core/grooves/rounds/round.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 import numpy as np
+from scipy.optimize import root_scalar, minimize_scalar
 
 from ..generic_elongation import GenericElongationGroove
 
@@ -6,17 +9,65 @@ from ..generic_elongation import GenericElongationGroove
 class RoundGroove(GenericElongationGroove):
     """Represents a round-shaped groove."""
 
-    def __init__(self, r1: float, r2: float, depth: float):
+    def __init__(
+            self,
+            r1: float,
+            r2: float,
+            depth: Optional[float] = None,
+            usable_width: Optional[float] = None,
+            pad_angle: float = 0,
+            sol_index: int = 0,
+    ):
         """
+        Give either ``depth`` or ``usable_width``.
+
         :param r1: radius 1 (face/flank)
         :param r2: radius 2 (flank/ground)
         :param depth: maximum depth
+        :param usable_width: usable width
+        :param pad_angle: angle between z-axis and the roll face padding
+        :param sol_index: If ``r2`` and ``usable_width`` are given there may be two valid solutions if ``2 * r2 < usable_width``.
+                Specify this index to choose one.
         """
+        pad_angle = np.deg2rad(pad_angle)
 
-        alpha = np.arccos(1 - depth / (r1 + r2))
-        usable_width = 2 * (r1 * np.sin(alpha) + r2 * np.sin(alpha) - r1 * np.tan(alpha / 2))
+        def l23(_alpha):
+            return r1 * np.tan((_alpha + pad_angle) / 2)
 
-        super().__init__(usable_width=usable_width, depth=depth, r1=r1, r2=r2, flank_angle=alpha)
+        if usable_width is None:
+            def f(_alpha):
+                return depth - r2 * (1 - np.cos(_alpha)) - l23(_alpha) * np.sin(_alpha)
+
+            alpha = root_scalar(f, bracket=(0, np.pi / 2)).root
+            usable_width = 2 * (l23(alpha) * np.cos(alpha) + r2 * np.sin(alpha))
+        elif depth is None:
+            def f(_alpha):
+                return usable_width / 2 - r2 * np.sin(_alpha) - l23(_alpha) * np.cos(_alpha)
+
+            if r2 * 2 <= usable_width:
+                m = minimize_scalar(f, bracket=(0, np.pi / 2)).x
+                alphas = root_scalar(f, bracket=(0, m)).root, root_scalar(f, bracket=(m, np.pi / 2)).root
+                alpha = alphas[sol_index]
+            else:
+                alpha = root_scalar(f, bracket=(0, np.pi / 2)).root
+
+            depth = l23(alpha) * np.sin(alpha) + r2 * (1 - np.cos(alpha))
+        elif r2 is None:
+            def f(_alpha):
+                return usable_width / 2 - (depth - l23(_alpha) * np.sin(_alpha)) / (1 - np.cos(_alpha)) * np.sin(
+                    _alpha
+                ) - l23(_alpha) * np.cos(_alpha)
+
+            alpha = root_scalar(f, bracket=(0, np.pi / 2)).root
+            r2 = (depth - l23(alpha) * np.sin(alpha)) / (1 - np.cos(alpha))
+
+        else:
+            raise TypeError("Give either usable_width or depth.")
+
+        super().__init__(
+            usable_width=usable_width, depth=depth, r1=r1, r2=r2, flank_angle=alpha,
+            pad_angle=pad_angle
+        )
 
     @property
     def types(self) -> '("round",)':
--- tests/grooves/test_round.py
+++ tests/grooves/test_round.py
@@ -1,15 +1,74 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import pytest
 from numpy import isclose, rad2deg
 
 from pyroll.core import RoundGroove
 
 
-def test_round():
-    g = RoundGroove(depth=15.55, r1=2, r2=15.8)
-
+def check(g):
+    assert isclose(g.depth, 15.55)
     assert isclose(g.usable_width, 31.79180677)
     assert isclose(rad2deg(g.alpha1), 82.738129)
     assert isclose(rad2deg(g.alpha2), 82.738129)
     assert isclose(g.z1, 17.65722232)
 
     assert not np.any(np.isclose(np.diff(g.contour_points[:, 0]), 0))  # test for duplicated points
+
+
+def test_round_depth():
+    g = RoundGroove(depth=15.55, r1=2, r2=15.8, pad_angle=0)
+
+    plt.figure(dpi=300)
+    plt.axes().set_aspect("equal")
+    plt.plot(*g.contour_line.xy)
+    plt.grid()
+    plt.axhline(15.55, c="k")
+    plt.show()
+    plt.close()
+
+    check(g)
+
+
+def test_round_r2():
+    g = RoundGroove(depth=15.55, usable_width=31.79180677, r1=2, pad_angle=0, r2=None)
+
+    plt.figure(dpi=300)
+    plt.axes().set_aspect("equal")
+    plt.plot(*g.contour_line.xy)
+    plt.grid()
+    plt.axhline(15.55, c="k")
+    plt.show()
+    plt.close()
+
+    check(g)
+
+
+def test_round_usable_width():
+    g1 = RoundGroove(usable_width=31.79180677, r1=2, r2=15.8, pad_angle=0, sol_index=0)
+
+    plt.figure(dpi=600)
+    plt.axes().set_aspect("equal")
+    plt.plot(*g1.contour_line.xy)
+    plt.axline((g1.z3, g1.y3), slope=-np.tan(g1.flank_angle), c="C0", lw=1, ls="--")
+    plt.axline((g1.z1, g1.y1), slope=np.tan(g1.pad_angle), c="C0", lw=1, ls="--")
+
+    check(g1)
+
+    g2 = RoundGroove(usable_width=31.79180677, r1=2, r2=15.8, pad_angle=0, sol_index=1)
+    plt.plot(*g2.contour_line.xy)
+    plt.axline((g2.z3, g2.y3), slope=-np.tan(g2.flank_angle), c="C1", lw=1, ls="--")
+    plt.axline((g2.z1, g2.y1), slope=np.tan(g2.pad_angle), c="C1", lw=1, ls="--")
+
+    plt.grid()
+    plt.axhline(15.55, c="k")
+    plt.ylim(-1, 18)
+    plt.show()
+    plt.close()
+
+
+def test_round_errors():
+    with pytest.raises(TypeError):
+        RoundGroove(depth=15.55, usable_width=31.79180677, r1=2, r2=15.8)
+    with pytest.raises(TypeError):
+        RoundGroove(r1=2, r2=15.8)
