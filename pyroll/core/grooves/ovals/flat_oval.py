28fa98a5ae707988c370566e5087ee4eda1185b3
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-02-14 08:09:24 +0100

    add even_ground_width arg to flat oval

--- pyroll/core/grooves/ovals/flat_oval.py
+++ pyroll/core/grooves/ovals/flat_oval.py
@@ -14,27 +14,38 @@ class FlatOvalGroove(GenericElongationGroove):
             r1: float,
             r2: float,
             depth: float,
-            usable_width: float,
+            usable_width: Optional[float] = None,
+            even_ground_width: Optional[float] = None,
             pad_angle: float = 0,
     ):
         """
-        Give exactly two of ``r2``, ``depth`` or ``usable_width``.
+        Give exactly one of ``usable_width`` and ``even_ground_width``.
 
         :param r1: radius 1 (face/flank)
         :param r2: radius 2 (flank/ground)
         :param depth: maximum depth
         :param usable_width: usable width
+        :param even_ground_width: width of the straight ground line
         :param pad_angle: angle between z-axis and the roll face padding
         """
         pad_angle = np.deg2rad(pad_angle)
 
         sol = solve_two_radii(r1=r1, r2=r2, depth=depth, width=None, pad_angle=pad_angle)
 
-        super().__init__(
-            r2=sol["r2"], depth=sol["depth"], usable_width=usable_width, flank_angle=sol["alpha"],
-            even_ground_width=usable_width - sol["width"],
-            r1=r1, pad_angle=pad_angle
-        )
+        if usable_width is None and even_ground_width is not None:
+            super().__init__(
+                r2=sol["r2"], depth=sol["depth"], usable_width=even_ground_width + sol["width"],
+                flank_angle=sol["alpha"], even_ground_width=even_ground_width,
+                r1=r1, pad_angle=pad_angle
+            )
+        elif even_ground_width is None and usable_width is not None:
+            super().__init__(
+                r2=sol["r2"], depth=sol["depth"], usable_width=usable_width, flank_angle=sol["alpha"],
+                even_ground_width=usable_width - sol["width"],
+                r1=r1, pad_angle=pad_angle
+            )
+        else:
+            raise TypeError("Give exactly one of usable_width and even_ground_width.")
 
     @property
     def types(self) -> '("oval", "flat_oval")':
--- tests/grooves/test_flat_oval.py
+++ tests/grooves/test_flat_oval.py
@@ -4,12 +4,21 @@ from numpy import isclose, rad2deg
 from pyroll.core import FlatOvalGroove
 
 
-def test_flat_oval():
-    g = FlatOvalGroove(depth=20, r1=5, r2=20, usable_width=60)
-
+def check(g):
+    assert isclose(g.usable_width, 60)
     assert isclose(g.even_ground_width, 9.58758548 * 2)
     assert isclose(rad2deg(g.alpha1), 78.463041)
     assert isclose(rad2deg(g.alpha2), 78.463041)
     assert isclose(g.z1, 34.0824829)
 
     assert not np.any(np.isclose(np.diff(g.contour_points[:, 0]), 0))  # test for duplicated points
+
+
+def test_flat_oval_uw():
+    g = FlatOvalGroove(depth=20, r1=5, r2=20, usable_width=60)
+    check(g)
+
+
+def test_flat_oval_egw():
+    g = FlatOvalGroove(depth=20, r1=5, r2=20, even_ground_width=9.58758548 * 2)
+    check(g)
