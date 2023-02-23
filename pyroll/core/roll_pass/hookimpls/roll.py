681c36ac231f70043385f3754ade063c8299e0bc
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-02-23 14:55:52 +0100

    add distinct contact length for square oval

--- pyroll/core/roll_pass/hookimpls/roll.py
+++ pyroll/core/roll_pass/hookimpls/roll.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 from ..roll_pass import RollPass
-from ..three_roll_pass import ThreeRollPass
-from ...config import GROOVE_PADDING
 
 
 @RollPass.Roll.roll_torque
@@ -12,18 +10,21 @@ def roll_torque(self: RollPass.Roll):
 
 @RollPass.Roll.contact_length
 def contact_length(self: RollPass.Roll):
-    """
-    Contact length between rolls and stock calculated using Siebel's approach
-    """
     height_change = self.roll_pass.in_profile.height - self.roll_pass.height
     return np.sqrt(self.min_radius * height_change - height_change ** 2 / 4)
 
 
+@RollPass.Roll.contact_length
+def contact_length_square_oval(self: RollPass.Roll):
+    if {"square", "box"}.intersection(self.roll_pass.in_profile.classifiers) and "oval" in self.roll_pass.classifiers:
+        depth = self.groove.local_depth(self.roll_pass.in_profile.width / 2)
+        height_change = self.roll_pass.in_profile.height - self.roll_pass.gap - 2 * depth
+        radius = self.max_radius - depth
+        return np.sqrt(radius * height_change - height_change ** 2 / 4)
+
+
 @RollPass.Roll.contact_area
 def contact_area(self: RollPass.Roll):
-    """
-    Contact area between rolls and stock calculated using Siebel's approach
-    """
     return (self.roll_pass.in_profile.width + self.roll_pass.out_profile.width) / 2 * self.contact_length
 
 
