1fa9cd881197b921853c3d07c6d39190e17ee13b
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-07-21 09:49:38 +0200

    Add default duration = 0 to Rotator (#70)
    add default duration = 0 to Rotator

--- pyroll/core/rotator/hookimpls.py
+++ pyroll/core/rotator/hookimpls.py
@@ -2,6 +2,11 @@ from .rotator import Rotator
 from shapely.affinity import rotate
 
 
+@Rotator.duration
+def duration(self: Rotator):
+    return 0
+
+
 @Rotator.OutProfile.cross_section
 def rotated_cross_section(self: Rotator.OutProfile):
     return rotate(self.rotator.in_profile.cross_section, angle=self.rotator.rotation, origin=(0, 0))
