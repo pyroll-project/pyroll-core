ebd08cee925635457b205795772fd6074de427a8
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-07-25 11:55:48 +0200

    reformat

--- pyroll/core/grooves/slitting.py
+++ pyroll/core/grooves/slitting.py
@@ -3,26 +3,27 @@ from numpy import tan, sin
 from shapely.geometry import LineString, Polygon
 from pyroll.core.grooves import GenericElongationGroove
 
+
 class SlittingGroove(GenericElongationGroove):
-    
+
     def __init__(
             self,
             groove,
             depth2
     ):
-        super().__init__(r1 = groove.r1, r2 = groove.r2, r3 = groove.r3, r4 = groove.r4,
-                         alpha1 = groove.alpha1, alpha2 = groove.alpha2, alpha3 = groove.alpha3,
-                         alpha4 = groove.alpha4, usable_width = groove.usable_width, depth = groove.depth, 
-                         even_ground_width = groove.even_ground_width, indent = groove.indent)
-        
+        super().__init__(r1=groove.r1, r2=groove.r2, r3=groove.r3, r4=groove.r4,
+                         alpha1=groove.alpha1, alpha2=groove.alpha2, alpha3=groove.alpha3,
+                         alpha4=groove.alpha4, usable_width=groove.usable_width, depth=groove.depth,
+                         even_ground_width=groove.even_ground_width, indent=groove.indent)
+
         self.flank_angle = np.arctan(groove._depthdepth / (groove.usable_width - groove.ground_width) * 2)
         self.y1 = depth2
         self.z1 = self.z2.copy()
 
         self.y2 = 0
-        #z2 is kept the same
+        # z2 is kept the same
 
-        self.y3 = self.r1*tan(self.alpha1)*sin(self.flank_angle)
+        self.y3 = self.r1 * tan(self.alpha1) * sin(self.flank_angle)
         self.z3 = self.z1 - sin(self.alpha1)
 
         self.y12 = self.y1 + self.r1
@@ -34,17 +35,17 @@ class SlittingGroove(GenericElongationGroove):
 
         self.test_plausibility()
 
-    def _generatepoints(self,groove):
-            right_side = np.array(list(self._enumerate_contour_points()))
-            left_side = np.flip(right_side, axis=0).copy()
-            left_side[:, 0] *= -1
-            list1 = np.concatenate([left_side, right_side[::-1]])
-            list2 = groove._contour_points.copy()
-            axisshift = abs(list1[-1,0])
-            list1[:,0] = list1[:,0] - axisshift
-            list2[:,0] = list2[:,0] + axisshift
-            return (np.concatenate([list2,list1]))
-        
-        
+    def _generatepoints(self, groove):
+        right_side = np.array(list(self._enumerate_contour_points()))
+        left_side = np.flip(right_side, axis=0).copy()
+        left_side[:, 0] *= -1
+        list1 = np.concatenate([left_side, right_side[::-1]])
+        list2 = groove._contour_points.copy()
+        axisshift = abs(list1[-1, 0])
+        list1[:, 0] = list1[:, 0] - axisshift
+        list2[:, 0] = list2[:, 0] + axisshift
+        return (np.concatenate([list2, list1]))
+
+
 '''RuntimeWarning: invalid value encountered in sqrt
   return self.y12 - sqrt(self.r1 ** 2 - (z - self.z12) ** 2)'''
