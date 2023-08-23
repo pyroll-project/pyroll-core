db39ecd8ca1270ad5b3cb9b30a3e219fb0549226
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-08-23 08:46:48 +0200

    add more elongations to PassSequence

--- pyroll/core/sequence/hookimpls.py
+++ pyroll/core/sequence/hookimpls.py
@@ -3,9 +3,24 @@ import numpy as np
 from .sequence import PassSequence
 
 
-@PassSequence.total_elongation
+@PassSequence.elongation
 def total_elongation(self: PassSequence):
     return self.in_profile.cross_section.area / self.out_profile.cross_section.area
+
+
+@PassSequence.log_elongation
+def total_log_elongation(self: PassSequence):
+    return np.log(self.elongation)
+
+
+@PassSequence.abs_elongation
+def total_abs_elongation(self: PassSequence):
+    return self.out_profile.length - self.in_profile.length
+
+
+@PassSequence.rel_elongation
+def total_rel_elongation(self: PassSequence):
+    return self.abs_elongation / self.in_profile.length
 
 
 @PassSequence.duration
