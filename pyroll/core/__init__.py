72b51238562a34ed49e382540343f9885a34ab1c
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-08-29 15:32:52 +0200

    incr version

--- pyroll/core/__init__.py
+++ pyroll/core/__init__.py
@@ -12,7 +12,7 @@ from .hooks import Hook, HookHost, HookFunction, root_hooks
 from .disk_elements import DiskElementUnit
 from .config import Config, config
 
-VERSION = "2.1.0"
+VERSION = "2.1.0-1"
 
 root_hooks.update(
     {
