342f152b4e610d1a3aaacad2487e7e00683c786e
Richard Pfeifer <richard.pfeifer@student.tu-freiberg.de>
2023-10-03 21:10:02 +0200

    Finalized groove and test file for the EquivalentRibbedGroove

--- pyroll/core/grooves/equivalent_ripped_groove.py
+++ pyroll/core/grooves/equivalent_ripped_groove.py
@@ -3,7 +3,7 @@ from typing import Optional
 import numpy as np
 
 from .generic_elongation import GenericElongationGroove
-from .generic_elongation_solvers import solve_r124
+from .generic_elongation_solvers import solve_r123
 
 
 class EquivalentRibbedGroove(GenericElongationGroove):
@@ -26,44 +26,66 @@ class EquivalentRibbedGroove(GenericElongationGroove):
     ):
         """
         All angles are measured in ° (degree).
-        Give exactly two of ``r2``, ``depth`` or ``usable_width``.
-        Give exactly one of ``flank_angle``, ``flank_width``, ``flank_height`` or ``flank_length``.
 
         :param r1: radius 1 (face/flank)
-        :param r2: radius 2 (flank/ground)
-        :param depth: maximum depth
+        :param r3: radius 3 (ground)
+        :param rib_distance: distance between two similar points on two consecutive ribs along the length of the rolled stock
+        :param rib_width: rib width
+        :param rib_angle: angle between the ribs and an axis along the length of the rolled stock
+        :param base_body_height: distance between two parallel faces of the rolled stocks base body
+        :param nominal_outer_diameter: the maximum outer diameter
         :param usable_width: usable width
+        :param depth: maximum depth
 
-        :param flank_angle: inclination angle of the flanks
-        :param flank_width: horizontal extent of the flanks
-        :param flank_height: vertical extent of the flanks
-        :param flank_length: length of the flanks
+
         :param pad_angle: angle between z-axis and the roll face padding
         :param kwargs: more keyword arguments passed to the GenericElongationGroove constructor
         """
+
         pad_angle = np.deg2rad(pad_angle)
+        rib_angle = np.deg2rad(rib_angle)
 
-        if rib_angle is not None:
-            rib_angle = np.deg2rad(rib_angle)
+        # Calculation of the radius 2, that makes the equivalent ribbed grove have the same average cross-section,
+        # as the non approximated
 
+        # rib width along the length of the rolled stock
         vertical_rib_width = rib_width / np.cos(rib_angle)
+
+        # ratio of rib width to rib distance
         width_distance_ratio = vertical_rib_width / rib_distance
+
         nominal_outer_radius = nominal_outer_diameter / 2
+
+        # height of the circle segment on top of the base body
         ribbed_circle_segment_height = nominal_outer_radius - base_body_height / 2
+
+        # height of the equivalent circle segment on top of the base body
         equivalent_circle_segment_height = ribbed_circle_segment_height * width_distance_ratio
 
-        r2 = (4 * equivalent_circle_segment_height ** 2 + base_body_height ** 2) / (
-                    8 * equivalent_circle_segment_height)
-        alpha = np.deg2rad(45)
 
+        # diagonal width of the base body
+        base_body_diagonal_width = base_body_height * np.sqrt(2)
 
+        # help triangle - interim calculation for later use
+        help_triangle_inner_angle = 180 - (45 + (180 - np.rad2deg(np.arcsin(((base_body_diagonal_width / 2) *
+                                    np.sin(np.deg2rad(45))) / nominal_outer_radius))))
+
+        # base width of the new circle segment on top of the base body
+        circle_segment_base_width = base_body_height - (2 * ((nominal_outer_radius *
+                                    np.sin(np.deg2rad(help_triangle_inner_angle))) / np.sin(np.deg2rad(45))))
+
+        # r2 calculated from the base width and height of the new circle segment
+        r2 = (4 * equivalent_circle_segment_height ** 2 + circle_segment_base_width ** 2) / (
+            8 * equivalent_circle_segment_height)
+
+        sol = solve_r123(r1=r1, r2=r2, r3=r3, depth=depth, width=usable_width, pad_angle=pad_angle)
 
         super().__init__(
-            r2=r2, depth=depth, usable_width=width, flank_angle=alpha,
-            r1=r1, pad_angle=pad_angle,
+            r2=r2, depth=depth, usable_width=usable_width, r1=r1, pad_angle=pad_angle, r3=r3,
+            alpha3=sol["alpha3"], flank_angle=sol["flank_angle"],
             **kwargs
         )
 
     @property
     def classifiers(self):
-        return {"round", "false_round"} | super().classifiers
+        return {"equivalent_ribbed", "round", "ribbed"} | super().classifiers
---
+++ tests/grooves/test_equivalent_ribbed_groove.py
@@ -0,0 +1,32 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from numpy import pi, isclose, rad2deg
+
+from pyroll.core import EquivalentRibbedGroove
+
+
+def check(g):
+    plt.figure(dpi=300)
+    plt.axes().set_aspect("equal")
+    plt.plot(*g.contour_points.T)
+    plt.show()
+
+    assert isclose(g.r1, 0.2)
+    assert isclose(g.r3, 3.45)
+    assert isclose(g.usable_width, 13.6788)
+    assert isclose(g.depth, 5.5091)
+
+    assert isclose(g.r2, 24.0753)
+
+
+    assert not np.any(np.isclose(np.diff(g.contour_points[:, 0]), 0))  # test for duplicated points
+
+
+def test_equivalent_ribbed_groove_r2():
+    g = EquivalentRibbedGroove(
+        r1=0.2, r3=3.45, rib_distance=8.4, rib_width=1.6, rib_angle=45, base_body_height=11.78,
+        nominal_outer_diameter=14, usable_width=13.6788, depth=5.5091
+    )
+    check(g)
+
+
