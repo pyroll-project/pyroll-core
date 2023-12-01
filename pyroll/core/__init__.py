3ec4af81bf0a8f35dc0b36699315bec3b60d2692
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2023-12-01 13:39:56 +0100

    updated version number

--- pyroll/core/__init__.py
+++ pyroll/core/__init__.py
@@ -12,7 +12,7 @@ from .hooks import Hook, HookHost, HookFunction, root_hooks
 from .disk_elements import DiskElementUnit
 from .config import Config, config
 
-VERSION = "2.1.2.post0"
+VERSION = "2.1.3"
 
 root_hooks.update(
     {
