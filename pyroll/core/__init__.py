affc862398cacf1d55b9e69ea8d0a50c544136af
Max Weiner <max.weiner@posteo.de>
2024-03-21 12:11:21 +0100

    bump version to post 1

--- pyroll/core/__init__.py
+++ pyroll/core/__init__.py
@@ -12,7 +12,7 @@ from .hooks import Hook, HookHost, HookFunction, root_hooks
 from .disk_elements import DiskElementUnit
 from .config import Config, config
 
-VERSION = "2.1.4"
+VERSION = "2.1.4-1"
 
 root_hooks.extend(
     [
