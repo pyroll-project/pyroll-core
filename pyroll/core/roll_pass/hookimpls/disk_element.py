6f37bbc30aec67fa0bf8e3f92c8322ccd3d453e2
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-04-15 15:56:36 +0200

    added angle implementation for disk elements and added tests

--- pyroll/core/roll_pass/hookimpls/disk_element.py
+++ pyroll/core/roll_pass/hookimpls/disk_element.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ..roll_pass import RollPass
 
 
@@ -14,3 +15,13 @@ def disk_velocity(self: RollPass.DiskElement):
 @RollPass.DiskElement.duration
 def disk_duration(self: RollPass.DiskElement):
     return self.length / self.velocity
+
+
+@RollPass.DiskElement.InProfile.roll_angle
+def roll_angle(self: RollPass.DiskElement.InProfile):
+    return np.arcsin(self.x / self.roll_pass.roll.working_radius)
+
+
+@RollPass.DiskElement.OutProfile.roll_angle
+def roll_angle(self: RollPass.DiskElement.OutProfile):
+    return np.arcsin(self.x / self.roll_pass.roll.working_radius)
