b3a53ec8346c08d0d16ba536c0e8227d9a9fc7cf
Richard Pfeifer <114106640+RichardPfr@users.noreply.github.com>
2024-07-15 14:03:04 +0200

    Changed and added rotations in the contour_lines property of the three_roll_pass.py

--- pyroll/core/roll_pass/three_roll_pass.py
+++ pyroll/core/roll_pass/three_roll_pass.py
@@ -21,13 +21,13 @@ class ThreeRollPass(RollPass):
             return self._contour_lines
 
         shift = self.roll.groove.usable_width / 2 / np.sqrt(3) + self.gap / np.sqrt(3)
-        upper = translate(self.roll.contour_line, yoff=shift)
-        upper = LineString(upper.coords[::-1])  # get back coordinate order
-        right = rotate(upper, angle=-60, origin=(0, 0))
-        left = rotate(upper, angle=60, origin=(0, 0))
-        upper = rotate(upper, angle=180, origin=(0, 0))
+        lower = translate(self.roll.contour_line, yoff=shift)
+        lower = LineString(lower.coords[::-1])  # get back coordinate order
+        right = rotate(lower, angle=-60, origin=(0, 0))
+        left = rotate(lower, angle=60, origin=(0, 0))
+        lower = rotate(lower, angle=180, origin=(0, 0))
 
-        self._contour_lines = [right, upper, left]
+        self._contour_lines = [right, left, lower]
         return self._contour_lines
 
     @property
