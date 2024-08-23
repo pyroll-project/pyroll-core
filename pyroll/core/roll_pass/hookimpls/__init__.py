3c0cf5a9ba34ade2d1cf329a8b8e1616060247f2
Max Weiner <max.weiner@posteo.de>
2024-08-23 14:34:00 +0200

    split roll pass hooks in multiple modules

--- pyroll/core/roll_pass/hookimpls/__init__.py
+++ pyroll/core/roll_pass/hookimpls/__init__.py
@@ -1,4 +1,6 @@
+from . import base_roll_pass
 from . import roll_pass
+from . import three_roll_pass
 from . import profile
 from . import roll
 from . import disk_element
---
+++ pyroll/core/roll_pass/hookimpls/base_roll_pass.py
@@ -0,0 +1,146 @@
+import numpy as np
+from shapely import difference
+from shapely.ops import linemerge
+
+from ..base import BaseRollPass
+from ...config import Config
+from ...rotator import Rotator
+
+
+@BaseRollPass.rotation
+def auto_rotation(self: BaseRollPass):
+    return Config.ROLL_PASS_AUTO_ROTATION
+
+
+@BaseRollPass.rotation
+def detect_already_rotated(self: BaseRollPass):
+    if Config.ROLL_PASS_AUTO_ROTATION and self.parent is not None:
+        try:
+            prev = self.prev
+        except IndexError:
+            return True
+
+        while True:
+            if isinstance(prev, BaseRollPass):
+                return True
+            if isinstance(prev, Rotator):
+                return False
+            try:
+                prev = prev.prev
+            except IndexError:
+                return True
+
+
+@BaseRollPass.orientation
+def default_orientation(self: BaseRollPass):
+    return 0
+
+
+@BaseRollPass.roll_force
+def roll_force(self: BaseRollPass):
+    return (self.in_profile.flow_stress + 2 * self.out_profile.flow_stress) / 3 * self.roll.contact_area
+
+
+@BaseRollPass.volume
+def volume(self: BaseRollPass):
+    return (self.in_profile.cross_section.area + 2 * self.out_profile.cross_section.area
+            ) / 3 * self.length
+
+
+@BaseRollPass.surface_area
+def surface_area(self: BaseRollPass):
+    return (self.in_profile.cross_section.perimeter + 2 * self.out_profile.cross_section.perimeter
+            ) / 3 * self.length
+
+
+@BaseRollPass.velocity
+def velocity(self: BaseRollPass):
+    if self.roll.has_value("neutral_angle"):
+        return self.roll.working_velocity * np.cos(self.roll.neutral_angle)
+    else:
+        return self.roll.rotational_frequency * self.roll.working_radius * 2 * np.pi
+
+
+@BaseRollPass.duration
+def duration(self: BaseRollPass):
+    return self.length / self.velocity
+
+
+@BaseRollPass.length
+def length(self: BaseRollPass):
+    return self.roll.contact_length
+
+
+@BaseRollPass.displaced_cross_section
+def displaced_cross_section(self: BaseRollPass):
+    return difference(self.in_profile.cross_section, self.usable_cross_section)
+
+
+@BaseRollPass.reappearing_cross_section
+def reappearing_cross_section(self: BaseRollPass):
+    return difference(self.out_profile.cross_section, self.in_profile.cross_section)
+
+
+@BaseRollPass.elongation_efficiency
+def elongation_efficiency(self: BaseRollPass):
+    return 1 - self.reappearing_cross_section.area / self.displaced_cross_section.area
+
+
+@BaseRollPass.target_filling_ratio(trylast=True)
+def default_target_filling(self: BaseRollPass):
+    return 1
+
+
+@BaseRollPass.target_width
+def target_width_from_target_filling_ratio(self: BaseRollPass):
+    if self.has_value("target_filling_ratio"):
+        return self.target_filling_ratio * self.usable_width
+
+
+@BaseRollPass.target_filling_ratio
+def target_filling_ratio_from_target_width(self: BaseRollPass):
+    if self.has_set_or_cached("target_width"):
+        return self.target_width / self.usable_width
+
+
+@BaseRollPass.target_cross_section_area
+def target_cross_section_area_from_target_cross_section_filling_ratio(self: BaseRollPass):
+    if self.has_set_or_cached("target_cross_section_filling_ratio"):
+        return self.target_cross_section_filling_ratio * self.usable_cross_section.area
+
+
+@BaseRollPass.target_cross_section_filling_ratio
+def target_cross_section_filling_ratio_from_target_cross_section_area(self: BaseRollPass):
+    if self.has_value("target_cross_section_area"):  # important has_value for computing from target_width
+        return self.target_cross_section_area / self.usable_cross_section.area
+
+
+@BaseRollPass.entry_point
+def entry_point(self: BaseRollPass):
+    return - self.roll.contact_length
+
+
+@BaseRollPass.entry_angle
+def entry_angle(self: BaseRollPass):
+    if "square" in self.in_profile.classifiers and "oval" in self.classifiers:
+        depth = self.roll.groove.local_depth(self.in_profile.width / 2)
+        radius = self.roll.max_radius - depth
+        return np.arcsin(self.entry_point / radius)
+
+    return np.arcsin(self.entry_point / self.roll.min_radius)
+
+
+@BaseRollPass.exit_point
+def exit_point(self: BaseRollPass):
+    return 0
+
+
+@BaseRollPass.exit_angle
+def exit_angle(self: BaseRollPass):
+    return np.arcsin(self.exit_point / self.roll.working_radius)
+
+
+@BaseRollPass.Profile.contact_lines
+def contact_contour_lines(self: BaseRollPass.Profile):
+    rp = self.roll_pass
+    return [linemerge(cl.intersection(self.cross_section.exterior.buffer(1e-9))) for cl in rp.contour_lines]
