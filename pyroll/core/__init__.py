fc01ee85a09ca2f7933214a812ef51d52ebb1ff6
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-11-06 16:16:08 +0100

    fix missing import, bump.vers.

--- pyroll/core/__init__.py
+++ pyroll/core/__init__.py
@@ -12,7 +12,7 @@ from .hooks import Hook, HookHost, HookFunction, root_hooks
 from .disk_elements import DiskElementUnit
 from .config import Config, config
 
-VERSION = "3.0.0"
+VERSION = "3.0.0post1"
 
 root_hooks.extend(
     [
--- pyroll/core/roll_pass/__init__.py
+++ pyroll/core/roll_pass/__init__.py
@@ -2,6 +2,7 @@ from .base import BaseRollPass
 from .two_roll_pass import TwoRollPass
 from .three_roll_pass import ThreeRollPass
 from .deformation_unit import DeformationUnit
+from .symmetric_roll_pass import SymmetricRollPass
 
 from . import hookimpls
 
