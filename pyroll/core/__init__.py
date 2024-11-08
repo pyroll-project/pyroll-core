a98ce0c42704501aee0f0b8eef3c75e975fcec25
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-11-08 10:15:45 +0100

    bump. vers. post3

--- pyroll/core/__init__.py
+++ pyroll/core/__init__.py
@@ -12,7 +12,7 @@ from .hooks import Hook, HookHost, HookFunction, root_hooks
 from .disk_elements import DiskElementUnit
 from .config import Config, config
 
-VERSION = "3.0.0post2"
+VERSION = "3.0.0post3"
 
 root_hooks.extend(
     [
