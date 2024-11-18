1d8a9f228bbd3cfacbc253673e44c3064b13ae00
Max Weiner <max.weiner@imf.tu-freiberg.de>
2024-11-18 11:43:10 +0100

    fix default color seq error and add equivalent function for multiline… (#279)
    fix default color seq error and add equivalent function for multilinestrings

---
+++ tests/test_shapes.py
@@ -0,0 +1,57 @@
+import shapely
+import pyroll.core
+import matplotlib.pyplot as plt
+
+
+def test_plot_line_string_matplotlib(tmp_path):
+    geom = shapely.LineString([(0, 0), (1, 1)])
+    p = geom.plot_matplotlib()
+    p.savefig(tmp_path / "plot.png")
+
+
+def test_plot_multi_line_string_matplotlib(tmp_path):
+    geom = shapely.MultiLineString([[(0, 0), (1, 1)], [(1, 0), (2, 1)]])
+    p = geom.plot_matplotlib()
+    p.savefig(tmp_path / "plot.png")
+
+
+def test_plot_polygon_matplotlib(tmp_path):
+    geom = shapely.Polygon([(0, 0), (1, 1), (2, 0)])
+    p = geom.plot_matplotlib()
+    p.savefig(tmp_path / "plot.png")
+
+
+def test_plot_multi_polygon_matplotlib(tmp_path):
+    geom = shapely.MultiPolygon(
+        [
+            shapely.Polygon([(0, 0), (1, 1), (2, 0)]),
+            shapely.Polygon([(0, 2), (1, 1), (2, 2)]),
+        ]
+    )
+    p = geom.plot_matplotlib()
+    p.savefig(tmp_path / "plot.png")
+
+
+def test_plot_line_string_plotly(tmp_path):
+    geom = shapely.LineString([(0, 0), (1, 1)])
+    geom.plot_plotly().write_html(tmp_path / "plot.html")
+
+
+def test_plot_multi_line_string_plotly(tmp_path):
+    geom = shapely.MultiLineString([[(0, 0), (1, 1)], [(1, 0), (2, 1)]])
+    geom.plot_plotly().write_html(tmp_path / "plot.html")
+
+
+def test_plot_polygon_plotly(tmp_path):
+    geom = shapely.Polygon([(0, 0), (1, 1), (2, 0)])
+    geom.plot_plotly().write_html(tmp_path / "plot.html")
+
+
+def test_plot_multi_polygon_plotly(tmp_path):
+    geom = shapely.MultiPolygon(
+        [
+            shapely.Polygon([(0, 0), (1, 1), (2, 0)]),
+            shapely.Polygon([(0, 2), (1, 1), (2, 2)]),
+        ]
+    )
+    geom.plot_plotly().write_html(tmp_path / "plot.html")
