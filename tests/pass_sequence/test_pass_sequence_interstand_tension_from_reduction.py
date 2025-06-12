616012253f8005785fa7435b4776eff9c2920480
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2025-06-12 08:31:25 +0200

    playing around with values published by C. Overhagen for the test

--- tests/pass_sequence/test_pass_sequence_interstand_tension_from_reduction.py
+++ tests/pass_sequence/test_pass_sequence_interstand_tension_from_reduction.py
@@ -14,7 +14,7 @@ from pyroll.core import (
     CircularOvalGroove,
     PassSequence,
     root_hooks,
-    Rotator,
+    Rotator, FalseRoundGroove,
 )
 
 
@@ -22,7 +22,7 @@ def width(self: RollPass.OutProfile, cycle):
     if cycle:
         return None
 
-    return self.roll_pass.in_profile.width * self.roll_pass.draught**-0.5
+    return self.roll_pass.in_profile.width * self.roll_pass.draught ** -0.5
 
 
 def test_pass_sequence_interstand_calculation_with_reductions(tmp_path: Path, caplog):
@@ -32,12 +32,14 @@ def test_pass_sequence_interstand_calculation_with_reductions(tmp_path: Path, ca
         root_hooks.add(RollPass.OutProfile.width)
         root_hooks.add(Rotator.OutProfile.width)
 
+        reductions = np.array([1.241, 1.229, 1.276])
+
         in_profile = Profile.round(
-            diameter=30e-3,
+            diameter=17e-3,
             temperature=1200 + 273.15,
             material=["C45", "steel"],
             flow_stress=100e6,
-            elastic_modulus = 53e6
+            elastic_modulus=53e6
         )
 
         sequence = PassSequence(
@@ -45,10 +47,14 @@ def test_pass_sequence_interstand_calculation_with_reductions(tmp_path: Path, ca
                 RollPass(
                     label="Oval I",
                     roll=Roll(
-                        groove=CircularOvalGroove(depth=8e-3, r1=6e-3, r2=40e-3),
-                        nominal_radius=160e-3,
+                        groove=CircularOvalGroove(
+                            depth=4.35e-3,
+                            r1=1.8e-3,
+                            r2=18.5e-3
+                        ),
+                        nominal_radius=208e-3,
                     ),
-                    gap=2e-3,
+                    gap=2.05e-3,
                 ),
                 Transport(
                     label="I => II",
@@ -57,32 +63,41 @@ def test_pass_sequence_interstand_calculation_with_reductions(tmp_path: Path, ca
                 RollPass(
                     label="Round II",
                     roll=Roll(
-                        groove=RoundGroove(r1=1e-3, r2=12.5e-3, depth=11.5e-3),
-                        nominal_radius=160e-3,
+                        groove=FalseRoundGroove(
+                            r1=1.55e-3,
+                            r2=7.1e-3,
+                            flank_angle=60,
+                            depth=6e-3
+                        ),
+                        nominal_radius=208e-3,
                     ),
-                    gap=2e-3,
+                    gap=1.550e-3,
                 ),
                 Transport(label="II => III", length=1),
                 RollPass(
                     label="Oval III",
                     roll=Roll(
-                        groove=CircularOvalGroove(depth=6e-3, r1=6e-3, r2=35e-3),
-                        nominal_radius=160e-3,
+                        groove=CircularOvalGroove(
+                            depth=3.6e-3,
+                            r1=1.4e-3,
+                            r2=15e-3
+                        ),
+                        nominal_radius=208e-3,
                     ),
-                    gap=2e-3,
+                    gap=1.4e-3,
                 ),
             ]
         )
 
         try:
-            sequence.solve_interstand_tensions_with_given_velocity_ratios(
-                in_profile=in_profile, final_speed=3, velocity_ratios=np.array([1.25, 1.42, 1.3]))
+            sequence.solve_interstand_tensions_with_given_velocity_ratios(in_profile=in_profile, final_speed=20.47, velocity_ratios=reductions)
         finally:
             print("\nLog:")
             print(caplog.text)
 
-            root_hooks.remove_last(RollPass.OutProfile.width)
-            root_hooks.remove_last(Rotator.OutProfile.width)
+        root_hooks.remove_last(RollPass.OutProfile.width)
+        root_hooks.remove_last(Rotator.OutProfile.width)
+
 
     try:
         import pyroll.report
@@ -95,16 +110,16 @@ def test_pass_sequence_interstand_calculation_with_reductions(tmp_path: Path, ca
         webbrowser.open(report_file.as_uri())
 
     except ImportError:
-         pass
-    #
-    # velocities = [rp.velocity for rp in sequence.roll_passes]
-    # assert np.isclose(velocities[-1], 1.5).all()
-    #
-    # in_profile_velocities = np.zeros_like(sequence.roll_passes)
-    # for i in range(1, len(sequence.roll_passes)):
-    #     in_profile_velocities[i] = sequence.roll_passes[i].in_profile.velocity
-    #
-    # out_profile_velocities = np.asarray([rp.out_profile.velocity for rp in sequence.roll_passes])
-    #
-    # for i in range(1, len(sequence.roll_passes)):
-    #     assert np.isclose(in_profile_velocities[i], out_profile_velocities[i - 1], atol=1e-3)
+        pass
+#
+# velocities = [rp.velocity for rp in sequence.roll_passes]
+# assert np.isclose(velocities[-1], 1.5).all()
+#
+# in_profile_velocities = np.zeros_like(sequence.roll_passes)
+# for i in range(1, len(sequence.roll_passes)):
+#     in_profile_velocities[i] = sequence.roll_passes[i].in_profile.velocity
+#
+# out_profile_velocities = np.asarray([rp.out_profile.velocity for rp in sequence.roll_passes])
+#
+# for i in range(1, len(sequence.roll_passes)):
+#     assert np.isclose(in_profile_velocities[i], out_profile_velocities[i - 1], atol=1e-3)
