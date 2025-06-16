e777380889c1df183e4b0f353dcf4fb49f64ffc3
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2025-06-16 14:38:03 +0200

    updated test due to altered cross-section calculation

--- tests/pass_sequence/test_pass_sequence_forward_velocity_calculation.py
+++ tests/pass_sequence/test_pass_sequence_forward_velocity_calculation.py
@@ -111,6 +111,6 @@ def test_pass_sequence_velocity_calculation_forward(tmp_path: Path, caplog):
     assert np.isclose(roll_pass_exiting_velocities.all(), out_profile_velocities.all(), atol=1e-3)
     assert np.isclose(sequence.roll_passes[0].in_profile.velocity, 1, atol=1e-3)
 
-    assert np.isclose(roll_pass_exiting_velocities[0], 1.2438, atol=1e-3)
+    assert np.isclose(roll_pass_exiting_velocities[0], 1.2509, atol=1e-3)
     for i in range(1, len(sequence.roll_passes)):
         assert np.isclose(in_profile_velocities[i], out_profile_velocities[i - 1], atol=1e-3)
