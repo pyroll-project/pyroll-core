63bc20bef471bfe2e480441e790e22450aee151b
Richard Pfeifer <114106640+RichardPfr@users.noreply.github.com>
2025-09-15 10:43:26 +0200

    Fix false cs calculation for square in_profile (#324)
    * add correct handling of incoming square profiles in the cross_section calculation
    
    * adding test
    
    * redefined check to all square profiles, for usage of `from_groove` function to create profiles
    
    * added test from cad
    
    ---------
    
    Co-authored-by: Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>

---
+++ tests/test_square_bulging.py
@@ -0,0 +1,62 @@
+import logging
+from pathlib import Path
+from shapely import LineString
+import numpy as np
+
+from pyroll.core import (
+    Profile,
+    Roll,
+    RollPass,
+    SquareGroove,
+    CircularOvalGroove,
+    PassSequence, Rotator
+)
+
+in_profile = Profile.from_groove(
+    groove=SquareGroove(
+        usable_width=21.54e-3,
+        tip_depth=10.6e-3,
+        r1=5e-3,
+        r2=3e-3
+    ),
+    filling=0.98,
+    gap=3e-3,
+    temperature=1200 + 273.15,
+    strain=0,
+    material=["C45", "steel"],
+    flow_stress=100e6,
+    density=7.5e3,
+    specific_heat_capcity=690,
+)
+
+
+def test_solve(tmp_path: Path, caplog):
+    caplog.set_level(logging.INFO, logger="pyroll")
+
+    sequence = PassSequence(
+        [
+            RollPass(
+                label="Oval",
+                roll=Roll(
+                    groove=CircularOvalGroove(
+                        depth=4.43e-3,
+                        r1=6e-3,
+                        r2=25.5e-3
+                    ),
+                    nominal_radius=320,
+                ),
+                velocity=1,
+                gap=2e-3,
+            )
+
+        ]
+    )
+
+
+    sequence.solve(in_profile)
+
+    horizontal_line_at_zero = LineString([(-sequence[0].in_profile.width / 2, 0), (sequence[0].in_profile.width / 2, 0)])
+    intersection_at_y0 = sequence[0].in_profile.cross_section.intersection(horizontal_line_at_zero)
+
+    assert np.isclose(sequence.out_profile.width, sequence.out_profile.cross_section.width)
+    assert np.isclose(intersection_at_y0.length, 17.247e-3, atol=1e-3)
